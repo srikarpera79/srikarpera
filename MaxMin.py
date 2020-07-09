8444882939807ae82ac91e54b8ba910977d3faf9
srikarpera79 <srikarpera@gmail.com>
2020-07-09 15:50:53 +0530

    Add files via upload

---
+++ MaxMin.py
@@ -0,0 +1,33 @@
+l = []
+
+for i in range(5):
+    print('Enter %i element'%i)
+    l.append(int(input()))
+l.sort()
+minsum,maxsum =0,0
+for i in range(5):
+    print(l[i])
+
+    if l[i] != l[0]:
+        maxsum = maxsum + l[i]
+
+    if l[i] != l[4]:
+        minsum = minsum + l[i]
+
+print(maxsum, minsum)
+
+
+
+    
+
+
+
+ 
+
+
+
+
+
+
+
+
